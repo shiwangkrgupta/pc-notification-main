93804151c5415ef77920e41158d91fb4c8719d70
captainyt-max <swdcaptain@gmail.com>
2025-09-01 12:09:50 +0530

    added ping endpoint

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ const { campusActivityListener } = require("./listeners/campusActivityListener")
 const { classActivityListener } = require("./listeners/classActivityListener");
 const { noticeListener } = require("./listeners/noticeListener");
 
-// later import others like noticeListener, classActivityListener etc.
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 console.log("🚀 Pocket Campus backend running...");
 
 campusActivityListener();
 classActivityListener();
 noticeListener();
+
+
+// 🔥 ping endpoint
+app.get("/ping", (req, res) => {
+  res.send("ok");
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
